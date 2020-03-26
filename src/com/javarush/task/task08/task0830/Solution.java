43d74f675c89b0da6d1decd3e73a88a9c8cef3b2
Sergey Demidov <ssevod@gmail.com>
2020-03-26 21:01:57 +0300

    8.12

---
+++ src/com/javarush/task/task08/task0830/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task08.task0830;
+
+import javax.xml.transform.sax.SAXSource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Задача по алгоритмам
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = reader.readLine();
+        }
+
+        sort(array);
+
+        for (String word : array) {
+            System.out.println(word);
+        }
+    }
+
+    public static void sort(String[] array) throws IOException {
+        String temp;
+
+        for (int i = 0; i < 20; i++){
+            for (int j = 0; j < 20; j++){
+                if (isGreaterThan(array[j], array[i])){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    //Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+}
